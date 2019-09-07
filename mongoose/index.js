bbaa0fc5760f1663e6a448365a2e2f04527509a3
Ubuntu <ubuntu@ip-172-31-38-255.us-east-2.compute.internal>
2019-09-07 23:52:41 +0530

    server changes

--- mongoose/index.js
+++ mongoose/index.js
@@ -2,9 +2,8 @@
 const mongoos = require('mongoose');
 
 const mongoConfig = require('../config/config.json').mongoDb;
-// const mongoUri = 'mongodb://'+mongoConfig.username + ":" + mongoConfig.password + "@" + mongoConfig.host + '/' + mongoConfig.collection;
-
-const mongoUri = 'mongodb://127.0.0.1:27017/quizo_api';
+//const mongoUri = 'mongodb://'+mongoConfig.username + ":" + mongoConfig.password + "@" + mongoConfig.host + '/' + mongoConfig.collection;
+ const mongoUri = 'mongodb://localhost/quizo_api';
 
 mongoos.Promise = global.Promise;
 mongoos.connect(mongoUri, { useNewUrlParser: true });
